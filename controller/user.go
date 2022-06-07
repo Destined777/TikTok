d7e095a76c4ab2066de9f14bed5782255157b16b
Destined777 <695693802@qq.com>
2022-06-07 19:19:50 +0800

    fea: final

--- controller/user.go
+++ controller/user.go
@@ -7,19 +7,6 @@ import (
 	"net/http"
 )
 
-// usersLoginInfo use map to store user info, and key is username+password for demo
-// user data will be cleared every time the server starts
-// test data: username=zhanglei, password=douyin
-var usersLoginInfo = map[string]http_param.User{
-	"zhangleidouyin": {
-		Id:            1,
-		Name:          "zhanglei",
-		FollowCount:   10,
-		FollowerCount: 5,
-		IsFollow:      true,
-	},
-}
-
 type UserLoginResponse struct {
 	http_param.Response
 	UserId int64  `json:"user_id,omitempty"`
